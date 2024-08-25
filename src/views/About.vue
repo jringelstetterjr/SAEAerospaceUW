27d40bc61166edb6c9386e8da85df980fa041968
Joel Ringelstetter Jr <jringelstetterjr@yahoo.com>
2024-08-25 18:25:55 -0500

    update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="about">
       <h1>About Us</h1>
-      <p>This is the about page content.</p>
+        <p> At SAE (Society of Automotive Engineers International) Aero UW, we dedicate each year to designing, building and flying an 
+            advanced RC aircraft for the SAE Aero Design Competition. Our journey begins with research and securing funding, followed by meticulous design work to meet 
+            SAE requirements. As the year progresses, we transition to the build phase, where we fabricate and assemble the aircraft's components with precision. 
+            Rigorous testing ensures that our plane is ready for the competition. In April, we head to California to showcase our work against teams from around 
+            the world. This competition marks the culmination of our efforts, but it also sets the stage for continuous improvement and innovation as we prepare for the next year.
+        </p>
     </div>
 </template>
   
