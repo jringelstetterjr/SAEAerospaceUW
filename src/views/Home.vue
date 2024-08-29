ada657f8ae38009e34e02162ab33ae0ba06c57af
Joel Ringelstetter Jr <jringelstetterjr@yahoo.com>
2024-08-29 15:23:37 -0500

    added separator to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,13 @@
 <!-- src/views/Home.vue -->
 <template>
-    <div class="home">
-      <h1>Welcome to the Home Page</h1>
+    <div class="title">
+      <h1>UW Madison SAE Aerospace</h1>
       <p>This is the home page content.</p>
     </div>
+    <div class="content">
+      
+    </div>
+    <hr class="separator"/>
   </template>
   
   <script>
@@ -14,23 +18,29 @@
   
   <style>
   
-  .home {
+  .title {
     padding: 2rem;
     margin-top: 5rem;
     overflow-y: auto;
     margin-bottom: 0rem;
     box-sizing: border-box;
     flex: 1;
+    justify-content: center;
   }
   h1 {
     font-family: 'exo ', sans-serif;
     font-weight: 600;
+    text-align: center;
+    font-size: 55px;
+    font-style: italic;
   }
   p {
     font-family: 'exo ', sans-serif;
     font-size: 1.4rem;
     line-height: 1.4;
     font-weight: 450;
+    justify-content: center;
+    text-align: center;
   }
 
   </style>
