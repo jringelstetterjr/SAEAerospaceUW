16917d0503b0987b1bcd3109827106ad1cf4a146
Joel Ringelstetter Jr <jringelstetterjr@yahoo.com>
2024-09-09 20:37:04 -0500

    sponsor and team changes!

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -3,82 +3,96 @@
     <div class="team">
         <h1>Meet the Team</h1>
         <hr class="separator">
+        <h2>Administration</h2>
         <div class="lead">
             <div class="member">
                 <img src="../assets/images/team/aiden.jpeg" alt="Adion Brion">
-                <h2>Aiden Brion</h2>
-                <p>President</p>
+                <h3>Aiden Brion</h3>
+                <p>President, Secretary</p>
             </div>
             <div class="member">
-                <img src="../assets/images/team/pat.jpeg" alt="Member 1">
-                <h2>Patrick Boyle</h2>
+                <img src="../assets/images/team/kuehne.jpeg" alt="Member 2">
+                <h3>Jason Kuehne</h3>
                 <p>Vice President</p>
             </div>
         </div>
         <hr class="separator">
+        <h2>Build Team</h2>
+        <div class="lead">
+            <div class="member">
+                <img src="../assets/images/team/pat.jpeg" alt="Member 1">
+                <h3>Patrick Boyle</h3>
+                <p>Mechanical Director</p>
+            </div>
+        </div>
+        <hr class="separator-team">
         <div class="row">
             <div class="member">
                 <img src="../assets/images/team/benny.jpeg" alt="Member 1">
-                <h2>Ben Kitchel</h2>
-                <p>Build Lead</p>
-            </div>
-            <div class="member">
-                <img src="../assets/images/team/pothula.jpeg" alt="Member 2">
-                <h2>Kaasyap Pothula</h2>
-                <p>Secretary</p>
-            </div>
-            <div class="member">
-                <img src="../assets/images/team/frame.jpeg" alt="Member 1">
-                <h2>Alex Frame</h2>
-                <p>Marketing Lead</p>
-            </div>
-        </div>
-        <hr class="separator">
-        <div class="row">
-            <div class="member">
-                <img src="../assets/images/team/kuehne.jpeg" alt="Member 2">
-                <h2>Jason Kuehne</h2>
-                <p>Aerodynamics Lead</p>
-            </div>
-            <div class="member">
-                <img src="../assets/images/team/morris.jpeg" alt="Member 2">
-                <h2>Jacob Morris</h2>
-                <p>Airfoil Sub Lead</p>
-            </div>
-            <div class="member">
-                <img src="../assets/images/team/grubis.jpeg" alt="Member 2">
-                <h2>Sean Grubis</h2>
-                <p>Electronics Lead</p>
-            </div>
-        </div>
-        <hr class="separator">
-        <div class="row">
-            <div class="member">
-                <img src="../assets/images/team/barrett.jpeg" alt="Member 1">
-                <h2>Niklas Barrett</h2>
+                <h3>Ben Kitchel</h3>
                 <p>Structural Lead</p>
             </div>
             <div class="member">
-                <img src="../assets/images/team/weingart.jpeg" alt="Member 1">
-                <h2>TJ Weingart</h2>
-                <p>Build Sub Lead</p>
+                <img src="../assets/images/team/grubis.jpeg" alt="Member 2">
+                <h3>Sean Grubis</h3>
+                <p>Controls Lead</p>
             </div>
             <div class="member">
+                <img src="../assets/images/team/barrett.jpeg" alt="Member 1">
+                <h3>Niklas Barrett</h3>
+                <p>Aerodynamics Lead</p>
+            </div>
+        </div>
+        <hr class="separator-team">
+        <div class="row">
+            <div class="member">
                 <img src="../assets/images/team/stott.jpeg" alt="Member 1">
-                <h2>Nick Stott</h2>
-                <p>Landing Gear Lead</p>
+                <h3>Nick Stott</h3>
+                <p>Fuselage Lead</p>
+            </div>
+            <div class="member">
+                <img src="../assets/images/team/morris.jpeg" alt="Member 2">
+                <h3>Jacob Morris</h3>
+                <p>Fuselage Sub Lead</p>
+            </div>
+            <div class="member">
+                <img src="../assets/images/team/pothula.jpeg" alt="Member 2">
+                <h3>Kaasyap Pothula</h3>
+                <p>Landing Gear Co-Lead</p>
+            </div>
+        </div>
+        <hr class="separator-team">
+        <div class="row">
+            <div class="member">
+                <img src="../assets/images/team/weingart.jpeg" alt="Member 1">
+                <h3>TJ Weingart</h3>
+                <p>Landing Gear Co-Lead</p>
+            </div>
+            <div class="member">
+                <img src="../assets/images/team/nurse.jpeg" alt="Member 1">
+                <h3>Julius Nurse</h3>
+                <p>Controls Sub Lead</p>
             </div>
         </div>
         <hr class="separator">
-        <div class="row">
+        <h2>Business</h2>
+        <div class="lead">
             <div class="member">
-                <img src="../assets/images/team/nurse.jpeg" alt="Member 1">
-                <h2>Julius Nurse</h2>
-                <p>Modeling Team Lead</p>
+                <img src="../assets/images/team/frame.jpeg" alt="Member 1">
+                <h3>Alex Frame</h3>
+                <p>Business Director</p>
+            </div>
+        </div>
+        <hr class="separator-team">
+        <div class="row">        
+            <div class="member">
+                <img src="../assets/images/team/aiden.jpeg" alt="Adion Brion">
+                <h3>Aiden Brion</h3>
+                <p>Sponsorship and Fundraising Lead, Documentation Lead</p>
             </div>
             <!-- <div class="member">
                 <img src="../assets/images/team/stock.jpg" alt="Member 2">
-                <h2>Joel Ringelstetter Jr</h2>
+                <h3>Joel Ringelstetter Jr</h3>
                 <p>Web Design Lead</p>
             </div> -->
         </div>
@@ -144,13 +158,26 @@ h1 {
 h2 {
     font-family: 'exo', sans-serif;
     font-weight: 600;
+    font-size: 2.5rem;
+    margin-top: 2rem;
+}
+h3 {
+    font-family: 'exo', sans-serif;
+    font-weight: 600;
     line-height: 1;
 }
 
 .separator {
     width: 75%;
+    height: 3px;
+    background-color: #6b6464; /* Adjust color as needed */
+    margin: 1rem auto; /* Centers the line and adds spacing */
+    border: none;
+}
+.separator-team {
+    width: 75%;
     height: 2px;
-    background-color: #9b9494; /* Adjust color as needed */
+    background-color: rgb(230, 226, 226); /* Adjust color as needed */
     margin: 1rem auto; /* Centers the line and adds spacing */
     border: none;
 }
@@ -160,6 +187,12 @@ p {
     line-height: 1.1;
     font-weight: 200;
     text-align: center;
+}
+p.subtitle {
+    font-size: 13px;
+    font-style: italic;
+    font-weight: 200;
+    margin-top: -0.5rem;
 }
 
 @media (max-width: 768px) {
